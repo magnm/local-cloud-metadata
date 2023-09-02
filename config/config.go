1d5dd7728c300af5b919620cdca1641ee0197a53
Magnus <post@magnusmork.no>
2023-09-02 15:24:58 +0200

    ksa binding resolver config

--- config/config.go
+++ config/config.go
@@ -6,10 +6,20 @@ const (
 	GoogleMetadata MetadataType = "google"
 )
 
+type KsaBindingResolver string
+
+const (
+	// KsaBindingResolverCRD means using an in-cluster CRD to resolve the cloud service account bound to the KSA.
+	KsaBindingResolverCRD KsaBindingResolver = "crd"
+	// KsaBindingResolverCloud means finding the cloud service account by querying the cloud's IAM policy.
+	KsaBindingResolverCloud KsaBindingResolver = "cloud"
+)
+
 type Config struct {
-	Port      string       `env:"PORT" envDefault:"8080"`
-	Type      MetadataType `env:"TYPE" envDefault:"google"`
-	ProjectId string       `env:"PROJECT_ID"`
+	Port       string       `env:"PORT" envDefault:"8080"`
+	Type       MetadataType `env:"TYPE" envDefault:"google"`
+	ProjectId  string       `env:"PROJECT_ID,notEmpty"`
+	KsaBinding string       `env:"KSA_BINDING" envDefault:"crd"`
 }
 
 // Initialised by server/run.go
